e0b0b13aefe318194707360f4e1437a75317bc2c
carlos-cb <carlos_chubei@hotmail.com>
2016-07-31 00:54:57 +0800

    首页修改 添加联系方式弹窗

--- src/CelinaBundle/Resources/public/js/celina.js
+++ src/CelinaBundle/Resources/public/js/celina.js
@@ -6,3 +6,34 @@ $(function(){
             .show();
     }).keyup();
 })
+
+$(document).ready(function() {
+
+    /* This is basic - uses default settings */
+
+    $("a#single_image").fancybox();
+
+    /* Using custom settings */
+
+    $("a#inline").fancybox({
+        helpers : {
+            overlay : {
+                css : {
+                    'background' : 'rgba(40, 33, 27, 0.3)'
+                }
+            }
+        },
+        'hideOnContentClick': true
+    });
+
+    /* Apply fancybox to multiple items */
+
+    $("a.group").fancybox({
+        'transitionIn'	:	'elastic',
+        'transitionOut'	:	'elastic',
+        'speedIn'		:	600,
+        'speedOut'		:	200,
+        'overlayShow'	:	false
+    });
+
+});
